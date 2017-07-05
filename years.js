6237d467bbd7c0dcdeed86511b2a2d1db97fc33a
tr ringstaff <trringstaff@iComputer.local>
2017-07-05 16:58:17 -0400

    __clean up, finish documenting code

--- years.js
+++ years.js
@@ -1,8 +1,11 @@
 $(document).ready( function() {
 
+  /**
+  * function which generates a list of people with birth and death years
+  * @param {Number} n - number of people to generate
+  * @return {Array} array of objects containing birthYear and deathYear values
+  */
   function generatePeople(number) {
-
-    console.log('generate people');
     //list of people to return
     var people = [];
 
@@ -15,6 +18,7 @@ $(document).ready( function() {
       var max = 2000;
       var min = 1900;
 
+      //pick random birth and death years
       var birth = Math.floor(Math.random() * (max - min + 1)) + min;
       var death = Math.floor(Math.random() * (max - birth + 1)) + birth;
 
@@ -24,56 +28,61 @@ $(document).ready( function() {
       });
     }
 
-
     return people;
   }
 
-  var peopleNumber = 10;
+  var peopleNumber = 100;
   var peeps = generatePeople(peopleNumber);
 
   /*
-  * dictionary to hold all years from 1900-2000 in first column
-  * number of people alive in second column
-
+  * object to hold number of people alive by year
+  *
+  * key: year
+  * value: number of people alive
   */
-
-
   var allYears = {};
 
-  var tempList = [];
-
+  //iterate through all people
   peeps.forEach(function(person) {
-
+    //for each year this person is alive, increment that year
     for(var year = person.birthYear; year <= person.deathYear; year++) {
-
       //init value to 0 if undefined
       if(!allYears[year]) {
         allYears[year] = 0;
       }
 
+      //increment value for this year
       allYears[year]++;
     }
-
   });
 
+  //array to hold copy of sorted allYears
   var sortable = [];
 
   for(var year in allYears) {
     sortable.push([year, allYears[year]]);
   }
 
+  //sort array by second column (people alive)
   sortable.sort(function(a, b) {
     return a[1] - b[1];
   });
 
+  //most people alive value will be last in the array after sorting
   var mostAlive = sortable[sortable.length - 1][1];
-  var j = sortable.length - 1;
-
+  var pointer = sortable.length - 1;
   var yearsText = "";
-  while(sortable[j][1] === mostAlive) {
-    yearsText += " " + sortable[j][0];
-    j--;
+
+  //get every year which had the most people alive
+  while(sortable[pointer][1] === mostAlive) {
+    //format if multiple years
+    if(yearsText) {
+      yearsText += ", ";
+    }
+    
+    yearsText += sortable[pointer][0];
+    pointer--;
   }
 
-  $('#holder').text("Year(s) with most (" + mostAlive + ") alive: " + yearsText);
+  $('#holder').text("Year(s) with most people alive ( " + mostAlive + " people ): " + yearsText);
 });
